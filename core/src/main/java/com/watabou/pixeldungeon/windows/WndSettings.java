eb0f066dd486fbc124209aea4123bd5bf22311b9
安怡 <anyicomplex@protonmail.com>
2021-12-23 19:17:35 -0500

    Initial commit

---
+++ core/src/main/java/com/watabou/pixeldungeon/windows/WndSettings.java
@@ -0,0 +1,235 @@
+/*
+ * Pixel Dungeon
+ * Copyright (C) 2012-2015 Oleg Dolya
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+package com.watabou.pixeldungeon.windows;
+
+import com.badlogic.gdx.Application;
+import com.badlogic.gdx.Gdx;
+import com.watabou.noosa.Camera;
+import com.watabou.noosa.audio.Sample;
+import com.watabou.noosa.ui.Button;
+import com.watabou.pixeldungeon.Assets;
+import com.watabou.pixeldungeon.PixelDungeon;
+import com.watabou.pixeldungeon.scenes.PixelScene;
+import com.watabou.pixeldungeon.ui.CheckBox;
+import com.watabou.pixeldungeon.ui.RedButton;
+import com.watabou.pixeldungeon.ui.Toolbar;
+import com.watabou.pixeldungeon.ui.Window;
+
+public class WndSettings extends Window {
+	
+	private static final String TXT_ZOOM_IN			= "+";
+	private static final String TXT_ZOOM_OUT		= "-";
+	private static final String TXT_ZOOM_DEFAULT	= "Default Zoom";
+
+	private static final String TXT_SCALE_UP		= "Scale up UI";
+	private static final String TXT_IMMERSIVE		= "Immersive mode";
+	
+	private static final String TXT_MUSIC	= "Music";
+	
+	private static final String TXT_SOUND	= "Sound FX";
+
+	private static final String TXT_BINDINGS	= "Key bindings";
+	
+	private static final String TXT_BRIGHTNESS	= "Brightness";
+	
+	private static final String TXT_QUICKSLOT	= "Second quickslot";
+	
+	private static final String TXT_SWITCH_PORT	= "Switch to portrait";
+	private static final String TXT_SWITCH_LAND	= "Switch to landscape";
+
+	private static final String TXT_SWITCH_FULL = "Switch to fullscreen";
+	private static final String TXT_SWITCH_WIN = "Switch to windowed";
+	
+	private static final int WIDTH		= 112;
+	private static final int BTN_HEIGHT	= 20;
+	private static final int GAP 		= 2;
+	
+	private RedButton btnZoomOut;
+	private RedButton btnZoomIn;
+	
+	public WndSettings( boolean inGame ) {
+		super();
+		
+		CheckBox btnImmersive = null;
+		
+		if (inGame) {
+			int w = BTN_HEIGHT;
+			
+			btnZoomOut = new RedButton( TXT_ZOOM_OUT ) {
+				@Override
+				protected void onClick() {
+					zoom( Camera.main.zoom - 1 );
+				}
+			};
+			add( btnZoomOut.setRect( 0, 0, w, BTN_HEIGHT) );
+			
+			btnZoomIn = new RedButton( TXT_ZOOM_IN ) {
+				@Override
+				protected void onClick() {
+					zoom( Camera.main.zoom + 1 );
+				}
+			};
+			add( btnZoomIn.setRect( WIDTH - w, 0, w, BTN_HEIGHT) );
+			
+			add( new RedButton( TXT_ZOOM_DEFAULT ) {
+				@Override
+				protected void onClick() {
+					zoom( PixelScene.defaultZoom );
+				}
+			}.setRect( btnZoomOut.right(), 0, WIDTH - btnZoomIn.width() - btnZoomOut.width(), BTN_HEIGHT ) );
+			
+			updateEnabled();
+			
+		} else {
+			
+			CheckBox btnScaleUp = new CheckBox( TXT_SCALE_UP ) {
+				@Override
+				protected void onClick() {
+					super.onClick();
+					PixelDungeon.scaleUp( checked() );
+				}
+			};
+			btnScaleUp.setRect( 0, 0, WIDTH, BTN_HEIGHT );
+			btnScaleUp.checked( PixelDungeon.scaleUp() );
+			add( btnScaleUp );
+
+			if (Gdx.app.getType() != Application.ApplicationType.WebGL) {
+				btnImmersive = new CheckBox( Gdx.app.getType() == Application.ApplicationType.Desktop ? resolutionText() : TXT_IMMERSIVE ) {
+					@Override
+					protected void onClick() {
+						super.onClick();
+						PixelDungeon.fullscreen( checked() );
+					}
+				};
+				btnImmersive.setRect( 0, btnScaleUp.bottom() + GAP, WIDTH, BTN_HEIGHT );
+				btnImmersive.checked( PixelDungeon.fullscreen() );
+				// FIXME
+				// btnImmersive.enable( android.os.Build.VERSION.SDK_INT >= 19 );
+				add( btnImmersive );
+			}
+
+		}
+		
+		CheckBox btnMusic = new CheckBox( TXT_MUSIC ) {
+			@Override
+			protected void onClick() {
+				super.onClick();
+				PixelDungeon.music( checked() );
+			}
+		};
+		btnMusic.setRect( 0, (btnImmersive != null ? btnImmersive.bottom() : BTN_HEIGHT) + GAP, WIDTH, BTN_HEIGHT );
+		btnMusic.checked( PixelDungeon.music() );
+		add( btnMusic );
+		
+		CheckBox btnSound = new CheckBox( TXT_SOUND ) {
+			@Override
+			protected void onClick() {
+				super.onClick();
+				PixelDungeon.soundFx( checked() );
+				Sample.INSTANCE.play( Assets.SND_CLICK );
+			}
+		};
+		btnSound.setRect( 0, btnMusic.bottom() + GAP, WIDTH, BTN_HEIGHT );
+		btnSound.checked( PixelDungeon.soundFx() );
+		add( btnSound );
+
+		Button lastBtn = btnSound;
+		
+		if (inGame) {
+			
+			CheckBox btnBrightness = new CheckBox( TXT_BRIGHTNESS ) {
+				@Override
+				protected void onClick() {
+					super.onClick();
+					PixelDungeon.brightness( checked() );
+				}
+			};
+			btnBrightness.setRect( 0, btnSound.bottom() + GAP, WIDTH, BTN_HEIGHT );
+			btnBrightness.checked( PixelDungeon.brightness() );
+			add( btnBrightness );
+			
+			CheckBox btnQuickslot = new CheckBox( TXT_QUICKSLOT ) {
+				@Override
+				protected void onClick() {
+					super.onClick();
+					Toolbar.secondQuickslot( checked() );
+				}
+			};
+			btnQuickslot.setRect( 0, btnBrightness.bottom() + GAP, WIDTH, BTN_HEIGHT );
+			btnQuickslot.checked( Toolbar.secondQuickslot() );
+			add( btnQuickslot );
+			
+			lastBtn = btnQuickslot;
+			
+		} else {
+
+			if (Gdx.app.getType() != Application.ApplicationType.Desktop) {
+				RedButton btnOrientation = new RedButton( orientationText() ) {
+					@Override
+					protected void onClick() {
+						PixelDungeon.landscape( !PixelDungeon.landscape() );
+					}
+				};
+				btnOrientation.setRect( 0, btnSound.bottom() + GAP, WIDTH, BTN_HEIGHT );
+				add( btnOrientation );
+
+				lastBtn = btnOrientation;
+			}
+			
+		}
+
+		if (Gdx.app.getType() == Application.ApplicationType.Desktop) {
+
+			RedButton btnKeymap = new RedButton(TXT_BINDINGS) {
+				@Override
+				protected void onClick() {
+					parent.add(new WndKeymap());
+				}
+			};
+			btnKeymap.setRect(0, lastBtn.bottom() + GAP, WIDTH, BTN_HEIGHT);
+			add(btnKeymap);
+
+			lastBtn = btnKeymap;
+		}
+
+		resize(WIDTH, (int) lastBtn.bottom());
+
+	}
+	
+	private void zoom( float value ) {
+
+		Camera.main.zoom( value );
+		PixelDungeon.zoom( (int)(value - PixelScene.defaultZoom) );
+
+		updateEnabled();
+	}
+	
+	private void updateEnabled() {
+		float zoom = Camera.main.zoom;
+		btnZoomIn.enable( zoom < PixelScene.maxZoom );
+		btnZoomOut.enable( zoom > PixelScene.minZoom );
+	}
+	
+	private String orientationText() {
+		return PixelDungeon.landscape() ? TXT_SWITCH_PORT : TXT_SWITCH_LAND;
+	}
+
+	private String resolutionText() {
+		return Gdx.graphics.isFullscreen() ? TXT_SWITCH_WIN : TXT_SWITCH_FULL;
+	}
+}
---
+++ lwjgl3/src/main/java/com/watabou/pixeldungeon/lwjgl3/Lwjgl3Launcher.java
@@ -0,0 +1,88 @@
+package com.watabou.pixeldungeon.lwjgl3;
+
+import com.badlogic.gdx.Files;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Preferences;
+import com.badlogic.gdx.utils.SharedLibraryLoader;
+import com.watabou.input.NoosaInputProcessor;
+import com.watabou.pixeldungeon.PixelDungeon;
+import com.watabou.pixeldungeon.Preferences;
+import com.watabou.utils.PDPlatformSupport;
+
+import java.io.File;
+
+/** Launches the desktop (LWJGL3) application. */
+public class Lwjgl3Launcher {
+	public static void main(String[] args) {
+		createApplication();
+	}
+
+	private static Lwjgl3Application createApplication() {
+		String version = Lwjgl3Launcher.class.getPackage().getSpecificationVersion();
+		if (version == null) {
+			version = "???";
+		}
+		String storageBasePath;
+		String storageRelativePath = "";
+		if (SharedLibraryLoader.isWindows) {
+			File appDataDir = new File(System.getenv("APPDATA"));
+			storageBasePath = appDataDir.getParent();
+			storageRelativePath = appDataDir.getName();
+			storageRelativePath += "/Watabou";
+		}
+		else {
+			storageBasePath = System.getProperty("user.home");
+		}
+		if (!storageBasePath.endsWith("/")) storageBasePath += "/";
+		if (SharedLibraryLoader.isLinux) {
+			storageRelativePath = ".local/share/watabou";
+		}
+		if (SharedLibraryLoader.isMac) {
+			storageRelativePath = "Library/Application Support/Watabou";
+		}
+		storageRelativePath += "/" + "Pixel Dungeon" + "/";
+		Lwjgl3ApplicationConfiguration configuration = new Lwjgl3ApplicationConfiguration();
+
+		configuration.setPreferencesConfig(storageRelativePath, Files.FileType.External);
+		// FIXME: This is a hack to get access to the preferences before we have an application setup
+		com.badlogic.gdx.Preferences prefs = new Lwjgl3Preferences(Preferences.FILE_NAME, storageBasePath + storageRelativePath);
+		boolean isFullscreen = prefs.getBoolean(Preferences.KEY_WINDOW_FULLSCREEN, false);
+
+		configuration.setWindowSizeLimits(Preferences.DEFAULT_WINDOW_WIDTH, Preferences.DEFAULT_WINDOW_HEIGHT, -1, -1);
+		if (isFullscreen) {
+
+		}
+		else {
+			int width = prefs.getInteger(Preferences.KEY_WINDOW_WIDTH, Preferences.DEFAULT_WINDOW_WIDTH);
+			int height = prefs.getInteger(Preferences.KEY_WINDOW_HEIGHT, Preferences.DEFAULT_WINDOW_HEIGHT);
+			configuration.setWindowedMode(width, height);
+		}
+		configuration.setWindowIcon(Files.FileType.Internal,
+				"ic_launcher_128.png", "ic_launcher_32.png", "ic_launcher_16.png");
+		configuration.useVsync(true);
+		//// Limits FPS to the refresh rate of the currently active monitor.
+		configuration.setForegroundFPS(Lwjgl3ApplicationConfiguration.getDisplayMode().refreshRate);
+		//// If you remove the above line and set Vsync to false, you can get unlimited FPS, which can be
+		//// useful for testing performance, but can also be very stressful to some hardware.
+		//// You may also need to configure GPU drivers to fully disable Vsync; this can cause screen tearing.
+
+		// TODO: It have to be pulled from build.gradle, but I don't know how it can be done
+		configuration.setTitle("Pixel Dungeon");
+		return new Lwjgl3Application(new PixelDungeon(
+				new Lwjgl3Support(version, storageRelativePath, new Lwjgl3InputProcessor())
+		), configuration);
+	}
+
+	private static class Lwjgl3Support extends PDPlatformSupport {
+		public Lwjgl3Support( String version, String basePath, NoosaInputProcessor inputProcessor ) {
+			super( version, basePath, inputProcessor );
+		}
+
+		@Override
+		public boolean isFullscreenEnabled() {
+			//	return Display.getPixelScaleFactor() == 1f;
+			return !SharedLibraryLoader.isMac;
+		}
+	}
+}
